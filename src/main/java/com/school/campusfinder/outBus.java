6b8b68138aadd7e53ac65832b60394f228e26215
suminhyun <115651196+Hsumina@users.noreply.github.com>
2024-11-07 20:15:55 +0900

    1.base
    통학버스 시간표 부분 현재까지 부분입니다

---
+++ src/main/java/com/school/campusfinder/outBus.java
@@ -0,0 +1,14 @@
+package com.school.campusfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class outBus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out_bus);
+    }
+}
